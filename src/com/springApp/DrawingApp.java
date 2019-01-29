74250e2e7ad6dbd4ddcff4a46cc255030c92adff
diana <diana@LAPTOP-4A5UHI78>
2019-01-29 11:29:01 -0800

    update

--- src/com/springApp/DrawingApp.java
+++ src/com/springApp/DrawingApp.java
@@ -3,16 +3,18 @@ package com.springApp;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.springApp.dao.Triangle;
 import com.springApp.dao.Triangle_AutowiringDemo;
 
 public class DrawingApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	    
+		//BeanFactory factory = new XmlBeanFactory(resource)
+//		 ApplicationContext context= new ClassPathXmlApplicationContext("beanfactory.xml");
+//		 Triangle tringlebean= (Triangle) context.getBean("triangle");
+//	     tringlebean.draw();
 		ApplicationContext context= new ClassPathXmlApplicationContext("beanfactory_autowiring.xml");    
 		Triangle_AutowiringDemo tringlebean= (Triangle_AutowiringDemo) context.getBean("triangle_autowiringDemo");
 	    tringlebean.draw_autowiringDemo();
-
 	}
 
 }
