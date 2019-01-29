d715f5e8e6d6eec67dfade3479440ff1df359c86
diana <diana@LAPTOP-4A5UHI78>
2019-01-29 12:10:47 -0800

    Implementing shutdownhook for AbstractAppContext | InitialisingBean | DisposeableBean

--- src/com/springApp/DrawingApp.java
+++ src/com/springApp/DrawingApp.java
@@ -1,9 +1,10 @@
 package com.springApp;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.springApp.dao.Triangle_AutowiringDemo;
+import com.springApp.dao.Triangle_demoCallBacks;
 
 public class DrawingApp {
 
@@ -12,9 +13,19 @@ public class DrawingApp {
 //		 ApplicationContext context= new ClassPathXmlApplicationContext("beanfactory.xml");
 //		 Triangle tringlebean= (Triangle) context.getBean("triangle");
 //	     tringlebean.draw();
-		ApplicationContext context= new ClassPathXmlApplicationContext("beanfactory_autowiring.xml");    
-		Triangle_AutowiringDemo tringlebean= (Triangle_AutowiringDemo) context.getBean("triangle_autowiringDemo");
-	    tringlebean.draw_autowiringDemo();
+		
+//		ApplicationContext context= new ClassPathXmlApplicationContext("beanfactory_autowiring.xml");    
+//		Triangle_AutowiringDemo tringlebean= (Triangle_AutowiringDemo) context.getBean("triangle_autowiringDemo");
+//	    tringlebean.draw_autowiringDemo();
+		AbstractApplicationContext context= new ClassPathXmlApplicationContext("beanfactory.xml");
+		context.registerShutdownHook();
+		/*registerShutdownHook() API is available only for AbstractAppContext. 
+		*calling this api enables shutdown of all beans at the end of this main method
+		*if the bean has implemented DisposableBean, then, the Destroy method of that bean will be called with this api call on the context
+		**/
+		
+		Triangle_demoCallBacks tringlebean= (Triangle_demoCallBacks) context.getBean("triangle_demoCallBacks");
+	    tringlebean.draw();
 	}
 
 }
@@ -31,5 +42,18 @@ appContext.getBean('..') : com.springApp.dao.Point@3224f60b
 BeanNameAware implementation is used to retrieve names of beans in the spring container
 --------Example below to access the current Bean's name---------------------------------------
 CurrentBeanName : triangle_autowiringDemo
+
+
+
+output after implementing AbstractApplicationContext and call to registerShutdownHook:
+--------Output start--------------------
+INFO: Loading XML bean definitions from class path resource [beanfactory.xml]
+printing from : afterPropertiesSet() from Triangle_demoCallBacks.java
+PointA: (1,1)
+-----------------------------------------------
+Jan 29, 2019 11:58:01 AM org.springframework.context.support.ClassPathXmlApplicationContext doClose
+INFO: Closing org.springframework.context.support.ClassPathXmlApplicationContext@1a6c5a9e: startup date [Tue Jan 29 11:58:01 PST 2019]; root of context hierarchy
+printing from : destroy() from Triangle_demoCallBacks.java
+--------Output End--------------------
  * 
  */
---
+++ src/com/springApp/dao/Triangle_demoCallBacks.java
@@ -0,0 +1,38 @@
+package com.springApp.dao;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Triangle_demoCallBacks implements InitializingBean,DisposableBean{
+	
+	private Point pointA;
+	
+	public Point getPointA() {
+		return pointA;
+	}
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public void draw() {
+		System.out.println("PointA: (" + getPointA().getX() + "," + getPointA().getY()+")");
+		System.out.println("-----------------------------------------------");
+		
+	}
+
+	//mandatory to create default constructor, else results in org.springframework.beans.factory.BeanCreationException
+	public Triangle_demoCallBacks() {
+	}
+
+	//Method below is implementation for InitializingBean interface
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("printing from : afterPropertiesSet() from Triangle_demoCallBacks.java");		
+	}
+	
+	//Method below is implementation for DisposableBean interface
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("printing from : destroy() from Triangle_demoCallBacks.java");
+	}
+}
